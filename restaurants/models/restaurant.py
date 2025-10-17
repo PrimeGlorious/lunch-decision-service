ea0b2a3e5bb5a78b672cee28a11d36f1b3670d9e
PrimeGlorious <TheComboPunk@gmail.com>
2025-10-17 10:20:22 +0300

    add location field for restaurant model

--- restaurants/models/restaurant.py
+++ restaurants/models/restaurant.py
@@ -5,6 +5,7 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
     rating = models.FloatField(validators=[MaxValueValidator(5), MinValueValidator(0)])
+    location = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.name} [rating {self.rating}/5]"
